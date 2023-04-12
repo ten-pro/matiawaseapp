b90743eb7c01e0212caffa903a76abfb72ebda0d
ueda masanari <Doraemon123453@gmail.com>
2023-04-12 10:53:30 +0900

    Headerタイムアップ後以外完成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import styles from "@/styles/Header/Header.module.css";
 import NowTime from "@/components/Header/NowTime";
 import SunTimer from "@/components/Header/SunTimer";
 import CountDown from "./Header/CountDown";
 import Image from "next/image";
+import { useAtom } from 'jotai';
+import { nowBgimg, nowTime, maxTime } from '@/atom/bgimg';
 
 const Header = () => {
+
+  const [timer] = useAtom(nowTime);
+  const [max, setMax] = useAtom(maxTime);
+  const [bgimg, setBgimg] = useAtom(nowBgimg);
+
   const images = [
     "/images/Header/sougen.svg",
+    "/images/Header/daybreak.svg",
+    "/images/Header/rain.svg",
+    "/images/Header/toutyaku.svg",
   ];
 
   const [currentTime, setCurrentTime] = useState<string>("");
@@ -15,7 +25,6 @@ const Header = () => {
     x: 0,
     y: 0,
   });
-  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
 
   const handleTimeChange = useCallback((time: string) => {
     setCurrentTime(time);
@@ -25,13 +34,20 @@ const Header = () => {
     setSunPosition({ x, y });
   }, []);
 
-  const handleImageChange = useCallback(() => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  }, []);
+  useEffect(() => {
+    console.log(timer / max)
+    if(timer / max > 0.3){
+      setBgimg(0);
+    }else if(timer / max > 0){
+      setBgimg(1);
+    }else{
+      setBgimg(2);
+    }
+  }, [timer]);
 
   return (
-    <div className={styles.wrap} onClick={handleImageChange}>
-      <Image src={images[currentImageIndex]} className={styles.bgimg} alt="背景" width={50} height={50} priority/>
+    <div className={styles.wrap}>
+      <Image src={images[bgimg]} className={styles.bgimg} alt="背景" width={50} height={50} priority/>
       <NowTime onTimeChange={handleTimeChange} />
       <SunTimer onPositionChange={handlePositionChange} />
       <CountDown initialTime="01:30:00.00" />
