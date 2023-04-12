c1bc1cc577de8e0436efd36f761d87c4b94035ab
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-04-12 11:24:53 +0900

    frend,sakuseiのレンダリングOK！

--- src/components/frend/frendinput.tsx
+++ src/components/frend/frendinput.tsx
@@ -1,10 +1,14 @@
 import Styles from "@/styles/Frend.module.css";
 import { useState } from "react";
-const frendinput = () =>{
+const frendinput = (props:any) =>{
+
+  const sousin = (e:any) =>{
+    props.setfrendinput(e.target.value);
+  }
 
   return(
     <div >
-      <input type="text" placeholder="メールアドレス" className={Styles.input} />
+      <input type="text" placeholder="メールアドレス" className={Styles.input} onChange={sousin}/>
 
     </div>
   )
--- src/pages/frend.tsx
+++ src/pages/frend.tsx
@@ -5,6 +5,7 @@ import Tuika from "@/components/frend/tuika";
 import Top from "@/components/Top";
 import Hyouzi from "@/components/frend/hyouzi";
 import Frendinput from "@/components/frend/frendinput";
+import Kakuteibtn from "@/components/frend/kakuteibtn";
 import { useState } from "react";
 import React from "react";
 
@@ -12,11 +13,15 @@ function frend(){
 
   const[inputdiv,setinputdiv] = useState<boolean>(true);
   const[hyouzidiv,sethyouzidiv] = useState<boolean>(false);
+  const[tuikabtn,settuikabtn] = useState<boolean>(false);
   
   const hidediv = () =>{
     setinputdiv(false);
     sethyouzidiv(true);
+    settuikabtn(true);
   }
+
+  const [frendinput,setfrendinput] = useState<string>("");
 
   return(
     <div>
@@ -30,12 +35,18 @@ function frend(){
           </div>
           
           <div className={Styles.freinput_area} style={{display:inputdiv?'none':'block'}}>
-          <Frendinput/>
+          <Frendinput setfrendinput={setfrendinput}/>
           </div>
         </div>
         
       </div>
-      <Tuika hidediv={hidediv}/>
+      <div style={{display:inputdiv?'block':'none'}}>
+        <Tuika hidediv={hidediv} />
+      </div>
+      <div style={{display:tuikabtn?'block':'none'}}>
+        <Kakuteibtn />
+      </div>
+      
       <Top/>
     </div>
     
