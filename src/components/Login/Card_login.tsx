9d67c6218ed848e06be4b82fd000b77b4af85519
明石凜太 <jeffrey.bezos@icloud.com>
2023-04-15 12:14:33 +0900

    ログインJS完了

--- src/components/Login/Card_login.tsx
+++ src/components/Login/Card_login.tsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
 import { Inter } from 'next/font/google'
 import Styles from '@/styles/Login/Card_login.module.css'
-// import { pink } from '@/atom/Login'
-
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
 function Card() {
+const [name, setname] = useState<string>('')
+const [pass, setpass] = useState<string>('')
+const [error, seterror] = useState<boolean>(false)
 
-
+const try_login = async () => {
+  try {
+    const response = await axios.post(
+      'http://mp-class.chips.jp/matiawase/main.php',
+      {
+        login_user: '',
+        name: name,
+        pass: pass
+      },
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+    );
+    console.log(response.data)
+    if(response.data.login === true) {
+      localStorage.setItem('user_id', response.data.user_information.user_id)
+      seterror(false)
+      console.log(localStorage.getItem('user_id'))
+    } else {
+      seterror(true)
+      // console.log(response.data.login)
+    }
+  } catch(error) {
+    console.log(error)
+  }
+}
   return (
     
     <div className={Styles.div}>
         <div className={Styles.card}>
             <div className={Styles.title}>ログインしてください</div>
-            <input className={Styles.name} placeholder="ユーザー名"></input>
-            <input className={Styles.pass} placeholder="パスワード"></input>
-            <div className={Styles.forgotPass}>パスワードを忘れましたか？</div>
-            <button className={Styles.button}>ログイン</button>
+            <input className={Styles.name} placeholder="ユーザー名" value={ name } onChange={(e) => setname(e.target.value)}></input>
+            <input className={Styles.pass} placeholder="パスワード" value={ pass } onChange={(e) => setpass(e.target.value)}></input>
+            <div className={Styles.error} style={{ display: error? 'block' : 'none' }}>エラー：名前とパスワードを確認してください</div>
+            <button className={Styles.button} onClick={ try_login }>ログイン</button>
         </div>
     </div>
   );
--- src/components/Shinki/Card_shinki.tsx
+++ src/components/Shinki/Card_shinki.tsx
@@ -11,7 +11,7 @@ function Card() {
     const[pass, setpass] = useState<string>('')
     const[error, seterror] = useState<boolean>(false)
 
-    const fetchData = async () => {
+    const create_account = async () => {
       try {
         const response = await axios.post(
           'http://mp-class.chips.jp/matiawase/main.php',
@@ -36,8 +36,7 @@ function Card() {
           seterror(true)
           // console.log(response.data)
         }
-      } 
-      catch(error) {
+      } catch(error) {
         console.log(error)
       }
     };
@@ -50,7 +49,7 @@ function Card() {
             <input className={Styles.pass} placeholder="メールアドレス"  value={ mail } onChange={(e) => setmail(e.target.value)}/>
             <input className={Styles.pass} placeholder="パスワード" type="password" value={ pass } onChange={(e) => setpass(e.target.value)}/>
             <div className={Styles.error} style={{display: error? 'block' : 'none'}}>エラー：名前が重複しています</div>
-            <button className={ Styles.button }  onClick={ fetchData }>アカウント作成</button>
+            <button className={ Styles.button }  onClick={ create_account }>アカウント作成</button>
         </div>
     </div>
   );
