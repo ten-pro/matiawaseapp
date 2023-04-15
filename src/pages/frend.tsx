b91554fa0e8864d71b15f04d0ed8a27400b8d7a3
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-04-15 14:45:43 +0900

    frend表示、エラー苦戦中

--- src/pages/frend.tsx
+++ src/pages/frend.tsx
@@ -6,11 +6,16 @@ import Top from "@/components/Top";
 import Hyouzi from "@/components/frend/hyouzi";
 import Frendinput from "@/components/frend/frendinput";
 import Kakuteibtn from "@/components/frend/kakuteibtn";
-import { useEffect, useState } from "react";
+import { useEffect, useState,useCallback } from "react";
 import React from "react";
 import { error } from "console";
 import { frendlisttype,Name } from "./kanako";
 import axios from "axios";
+
+interface Friend {
+  friend_id: number;
+  friend_name: string;
+}
 
 function frend(){
 
@@ -30,25 +35,66 @@ function frend(){
   });
 
 
-  const [frendname, setFrendname] = useState<Name[]>([
-    { name: '上田' },
-    { name: '上田' },
-    { name: '上田' },
-    { name: '宮川' },
-    { name: '明石' },
-  ]);
+  const [frendname, setFrendname] = useState<Name[]>([]);
 
 
 
-  useEffect(() => {
-    const fetchData = async () => {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.post(
+  //         'http://mp-class.chips.jp/matiawase/main.php',
+  //         {
+  //           login_user: '',
+  //           name: '変更',
+  //           pass: 'pass0000',
+  //         },
+  //         {
+  //           headers: {
+  //             'Content-Type': 'multipart/form-data',
+  //           },
+  //         }
+  //       );
+  //       if (response.data === false) {
+  //         // userData.error1 = true;
+  //       } else {
+  //         console.log(response);
+  //         sessionStorage.setItem('id', response.data.user_information.user_id);
+  //         if(response.data.get_friendlist==null){
+  //           console.log("フレンドがいません");
+  //           let friend_id:number=0;
+  //           let friend_name:string="フレンドがいません。登録しませんか？";
+  //           //setallfrenddata({friend_id:friend_id ,friend_name:friend_name});
+  //           setFrendname((prevFrendname) => [...prevFrendname, { name: friend_name }]);
+  //           if (!response.data.get_frinedlist) { // 判定を追加する
+  //             return;
+  //           }
+  //         }else{
+  //         let friend_id:number=response.data.get_friendlist.friend_id;
+  //         let friend_name:string=response.data.get_friendlist.friend_name;
+  //         location.href;
+  //         setFrendname((prevFrendname) => [...prevFrendname, { name: friend_name }]);
+  //         }
+  //       }
+  //       console.log(response);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // 
+  
+  const fetchData = useCallback(() => {
+    const fetchDataAsync = async () => {
       try {
         const response = await axios.post(
           'http://mp-class.chips.jp/matiawase/main.php',
           {
             login_user: '',
-            name: '',
-            pass: '',
+            name: '変更',
+            pass: 'pass0000',
           },
           {
             headers: {
@@ -59,17 +105,53 @@ function frend(){
         if (response.data === false) {
           // userData.error1 = true;
         } else {
+          console.log(response);
           sessionStorage.setItem('id', response.data.user_information.user_id);
-          location.href;
+          if (response.data.get_friendlist === null) {
+            console.log('フレンドがいません');
+            let friend_id: number = 0;
+            let friend_name: string =
+              'フレンドがいません。登録しませんか？';
+            setallfrenddata({ friend_id: friend_id, friend_name: friend_name });
+            setFrendname((prevFrendname) => [
+              ...prevFrendname,
+              { name: friend_name },
+            ]);
+            if (!response.data.get_frinedlist) {
+              return;
+            }
+          } else {
+            if (Array.isArray(response.data.get_friendlist)) {
+              const friends = response.data.get_friendlist as Friend[];
+              friends.forEach((friend) => {
+                let friend_id: number = friend.friend_id;
+                let friend_name: string = friend.friend_name;
+                location.href;
+                setFrendname((prevFrendname) => [
+                  ...prevFrendname,
+                  { name: friend_name },
+                ]);
+              });
+             } else {
+              let friend_id: number = response.data.get_friendlist.friend_id;
+              let friend_name: string = response.data.get_friendlist.friend_name;
+              location.href;
+              setFrendname((prevFrendname) => [
+                ...prevFrendname,
+                { name: friend_name },
+              ]);
+             }
+          }
+          console.log(response);
         }
-        console.log(response);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchData();
+    fetchDataAsync();
   }, []);
-
+  
+  useEffect(fetchData, []);
 
   return(
     <div>
